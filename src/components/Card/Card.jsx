5f9765c4531957fab96996c4313b124f734da4dd
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-12-12 18:38:47 +0100

    aggiunta gestione delle bandiere per alcune lingue

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,5 @@
 
 
-
-
-
 export default function Card({ content }) {
 
     console.log(content)
@@ -11,9 +8,21 @@ export default function Card({ content }) {
     function getFlagUrl(original_language) {
         let url
         switch (original_language) {
-            case 'en': url = ''
-        }
+            case 'en': url = 'https://flagcdn.com/h20/gb.png';
+                break;
+            case 'it': url = 'https://flagcdn.com/h20/it.png';
+                break;
+            case 'fr': url = 'https://flagcdn.com/h20/fr.png';
+                break;
+            case 'es': url = 'https://flagcdn.com/h20/es.png';
+                break;
+            case 'ja': url = 'https://flagcdn.com/h20/jp.png';
+                break;
+            default: url = 'https://flagcdn.com/h20/gb.png';
+                break
 
+        }
+        return url
     }
 
 
@@ -25,7 +34,7 @@ export default function Card({ content }) {
         <div>
             <div>{title}</div>
             <div>{original_title}</div>
-            <div>{original_language}</div>
+            <img src={getFlagUrl(original_language)} alt="" />
             <div>{vote_average}</div>
         </div>
 
